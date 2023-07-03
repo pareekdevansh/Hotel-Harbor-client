2d43ab91a6d1409a247ebef2fd8268aee445f71e
Devansh Pareek <devanshpareek863@gmail.com>
2023-07-04 00:09:20 +0530

    removed vercel.json

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 module.exports = function (app) {
   app.use(
     "/api",
+    console.log("api url: ", process.env.SERVER_URL),
     createProxyMiddleware({
       target: process.env.SERVER_URL,
       changeOrigin: true,
